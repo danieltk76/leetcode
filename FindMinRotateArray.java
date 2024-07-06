5582264d673f6e534f383f84bccccbc87ef108bf
danieltk76 <knight.daniel0612@gmail.com>
2024-07-06 13:51:49 -0400

    leetcode 153

---
+++ FindMinRotateArray.java
@@ -0,0 +1,18 @@
+public class FindMinRotateArray 
+{
+    public int findMin(int[] nums)
+    {
+        int min = nums[0];
+
+        for (int i = 1; i < nums.length; i++)
+        {
+            if (nums[i] < min)
+            {
+                min = nums[i];
+            }
+        }
+        return min;
+        
+    }
+    
+}
